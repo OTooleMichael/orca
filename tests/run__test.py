f7fce51c132102eef2f45f763c56900ae6ae9f1f
Michael O Toole <otoole.timmy@gmail.com>
2024-03-24 18:14:20 +0000

    Test cleanup

--- tests/run__test.py
+++ tests/run__test.py
@@ -1,161 +1,28 @@
-from contextlib import closing, contextmanager
 import time
-from typing import Generator, Protocol
 import pytest
-from dataclasses import dataclass, field
 
-from orca_tools.py_event_server import emitter as _emitter
 from orca_tools.protos import Event
-from orca_tools.py_event_server import MemoryBus, EventBus
-from cerebro import Cerebro
 from orca_tools.task_server_utils import Server
-from task_servers import task_server_letters, task_server_nums
+from task_servers import task_server_nums
 from generated_grpc import orca_pb2 as pb2
 from generated_grpc import orca_enums as en
-
-
-class PatternFn(Protocol):
-    def __call__(self, event: Event) -> bool: ...
-
-
-@dataclass
-class TaskStateMatcher(PatternFn):
-    task_name: str
-    state: en.TaskState
-
-    def __call__(self, event: Event) -> bool:
-        if not isinstance(event, pb2.TaskStateEvent):
-            return False
-        return (
-            event.event.task_name == self.task_name and event.event.state == self.state
-        )
-
-
-def message_to_string(message: Event) -> str:
-    return "\n".join([message.DESCRIPTOR.name, str(message)])
-
-
-Pattern = list[list[PatternFn]]
-
-
-@dataclass
-class WaitConsumer:
-    targeted: PatternFn
-    pattern: Pattern
-    matched: list[Event] = field(default_factory=list)
-    seen: list[Event] = field(default_factory=list)
-    error: str = ""
-    debug: bool = False
-
-    def __repr__(self) -> str:
-        matched = "\n".join([message_to_string(message) for message in self.matched])
-        seen = "\n".join([message_to_string(message) for message in self.seen])
-        return f"{self.error} {self.empty=}\nMatched:\n{matched},\nSeen:\n{seen}"
-
-    @property
-    def empty(self) -> bool:
-        return not self.pattern and not self.error
-
-    def consume(self, event: Event) -> bool:
-        """Expects events to arrive in an order.
-        Closes thread when all patterns are matched,
-        or an unknown event is received.
-        """
-        return self._consume(event)
-
-    def handle(self, event: Event, emitter: EventBus) -> bool:
-        return self._consume(event)
-
-    def _consume(self, event: Event) -> bool:
-        if not self.targeted(event):
-            return False
-
-        if self.debug:
-            print(message_to_string(event))
-
-        self.seen.append(event)
-        current_layer = self.pattern.pop(0) if self.pattern else None
-        if not current_layer:
-            print("No more patterns to match")
-            return True
-
-        new_layer = list(filter(lambda pattern: not pattern(event), current_layer))
-        if len(new_layer) == len(current_layer):
-            self.error = f"No pattern matched for {event!s}"
-            print(self.error)
-            return True
-
-        self.matched.append(event)
-        if not new_layer:
-            return self.empty
-
-        self.pattern = [new_layer] + self.pattern
-        return False
-
-
-def _run_task(
-    task_name: str,
-    consumer: WaitConsumer,
-    timeout: int = 5,
-    emitter: EventBus = _emitter,
-) -> None:
-    """Run a given task and wait according to the pattern of our consumer."""
-    event = pb2.UserRunTaskEvent(
-        event=pb2.EventCore(task_name=task_name, source_server_id="cli"),
-    )
-    thread = emitter.subscribe_thread(consumer.handle, "consumer")
-    emitter.publish(event)
-    try:
-        thread.join(timeout=timeout)
-    except TimeoutError:
-        assert not consumer.empty, "Patterns matched but failed to exit thread"
-        raise AssertionError("Unmatched patterns")
-    assert not consumer.error, consumer
-    assert consumer.empty, consumer
+from tests.utils import (
+    Pattern,
+    TaskStateMatcher,
+    run_task,
+    WaitConsumer,
+    create_server,
+    NameMatcher,
+)
 
 
 def _is_state_event(event: Event) -> bool:
     return isinstance(event, pb2.TaskStateEvent)
 
 
-@dataclass
-class ServersStarted:
-    servers: list[str]
-
-    def __call__(self, event: Event, emitter: EventBus) -> bool:
-        if not isinstance(event, pb2.ServerStateEvent):
-            return False
-        if event.event.state != en.ServerState.READY:
-            return False
-        self.servers = [
-            server for server in self.servers if server != event.event.source_server_id
-        ]
-        return not self.servers
-
-
-@contextmanager
-def _create_server() -> Generator[MemoryBus, None, None]:
-    emitter = MemoryBus()
-    with closing(emitter):
-        cer = Cerebro(emitter=emitter)
-        tasks = task_server_letters.tasks
-        server = task_server_letters.Server(name="a", tasks=tasks, emitter=emitter)
-        ready_check = ServersStarted(
-            servers=[
-                cer.server_name,
-                server.name,
-            ]
-        )
-        thread = emitter.subscribe_thread(ready_check, "ready_check")
-        server.start()
-        cer.start()
-        thread.join(timeout=1)
-        yield emitter
-
-
 def test_passing() -> None:
-    with _create_server() as emitter:
-        _run_task(
+    with create_server() as emitter:
+        run_task(
             "task_d",
             WaitConsumer(
                 debug=True,
@@ -174,8 +41,8 @@ def test_bad_pattern() -> None:
         [TaskStateMatcher("task_d", en.TaskState.COMPLETED)],
         [TaskStateMatcher("task_d", en.TaskState.STARTED)],
     ]
-    with _create_server() as emitter, pytest.raises(AssertionError):
-        _run_task(
+    with create_server() as emitter, pytest.raises(AssertionError):
+        run_task(
             "task_d",
             WaitConsumer(
                 pattern=pattern,
@@ -185,18 +52,8 @@ def test_bad_pattern() -> None:
         )
 
 
-@dataclass
-class NameMatcher:
-    task_name: str
-
-    def __call__(self, event: Event) -> bool:
-        if not isinstance(event, pb2.TaskStateEvent):
-            return False
-        return event.event.task_name == self.task_name
-
-
 def test_task_a() -> None:
-    with _create_server() as emitter:
+    with create_server() as emitter:
         pattern: Pattern = [
             [
                 NameMatcher("task_b"),
@@ -206,7 +63,7 @@ def test_task_a() -> None:
                 NameMatcher("task_a"),
             ],
         ]
-        _run_task(
+        run_task(
             "task_a",
             WaitConsumer(
                 pattern=pattern,
@@ -227,12 +84,12 @@ def test_two_servers() -> None:
         [NameMatcher("task_a")],
         [NameMatcher("task_2")],
     ]
-    with _create_server() as emitter:
+    with create_server() as emitter:
         tasks = task_server_nums.server.tasks
         server = Server(name="b", tasks=tasks, emitter=emitter)
         server.start()
         time.sleep(1)
-        _run_task(
+        run_task(
             "task_2",
             WaitConsumer(
                 pattern=pattern,
@@ -247,8 +104,8 @@ def test_two_servers() -> None:
 def test_unknown_task() -> None:
     """Should emit and error when an unknown task is run."""
     pattern: Pattern = [[TaskStateMatcher("task_unknown", en.TaskState.NOT_EXISTING)]]
-    with _create_server() as emitter:
-        _run_task(
+    with create_server() as emitter:
+        run_task(
             "task_unknown",
             WaitConsumer(
                 pattern=pattern,
@@ -269,8 +126,8 @@ def test_requires_unknown_task() -> None:
             ),
         ]
     ]
-    with _create_server() as emitter:
-        _run_task(
+    with create_server() as emitter:
+        run_task(
             "task_requires_non_extisting_task",
             WaitConsumer(
                 pattern=pattern,
@@ -341,13 +198,12 @@ def test_failing_upstream() -> None:
         [TaskStateMatcher("task_requires_failing", en.TaskState.FAILED_UPSTREAM)],
     ]
 
-    with _create_server() as emitter:
-        _run_task(
+    with create_server() as emitter:
+        run_task(
             "task_requires_failing",
             WaitConsumer(
                 pattern=pattern,
                 targeted=lambda event: isinstance(event, pb2.TaskStateEvent),
-                debug=False,
             ),
             emitter=emitter,
         )
